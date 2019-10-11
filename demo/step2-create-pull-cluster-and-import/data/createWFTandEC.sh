e10017ced5ae1af39bc9a826dd15aad3bfcefa36
Andrea <andrea.fasola@gmail.com>
2019-10-11 12:39:54 +0200

    improved readme

--- demo/step2-create-pull-cluster-and-import/data/createWFTandEC.sh
+++ demo/step2-create-pull-cluster-and-import/data/createWFTandEC.sh
@@ -1,3 +1,3 @@
 gcloud dataproc workflow-templates create pull-cluster-template --region europe-west3
-gcloud dataproc workflow-templates set-managed-cluster pull-cluster-template --region europe-west3 --subnet gcp-target-hadoop-cluster-nw --zone "" --single-node --master-machine-type n1-standard-4 --master-boot-disk-size 500 --image-version 1.3-deb9 --project onprem2gcp --initialization-actions 'gs://onprem2gcp-gcp-target-data/pull.sh'
+gcloud dataproc workflow-templates set-managed-cluster pull-cluster-template --region europe-west3 --subnet gcp-target-hadoop-cluster-nw --zone "" --single-node --master-machine-type n1-standard-16 --master-boot-disk-size 500 --image-version 1.3-deb9 --project onprem2gcp --initialization-actions 'gs://onprem2gcp-gcp-target-data/pull.sh'
 gcloud dataproc workflow-templates add-job pyspark gs://onprem2gcp-gcp-target-data/empty.py --step-id empty --workflow-template pull-cluster-template --region europe-west3
